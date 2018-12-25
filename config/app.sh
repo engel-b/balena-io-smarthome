acccc933dc1e71a6ca93c221f29185be1076a1db
Björn Engel <bjoern.engel@cgi.com>
2018-12-25 13:54:52 +0100

    #

--- config/app.sh
+++ config/app.sh
@@ -3,3 +3,7 @@
 cp -r /config/asterisk/* /data/asterisk/.
 cp -r /config/owfs/* /data/owfs/.
 cp -r /config/openhab/* /data/openhab/.
+cp -r /config/mosquitto/* /data/mosquitto/.
+cp -r /config/signal/* /data/signal/.
+
+exit 0
