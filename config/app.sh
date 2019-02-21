18f3486d37ac7d5373b61425302e6215043a63ee
Björn Engel <bjoern.engel@cgi.com>
2019-02-21 21:16:07 +0100

    removing ssh from config container

--- config/app.sh
+++ config/app.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-cp -r /config/asterisk/* /data/asterisk/.
-cp -r /config/owfs/* /data/owfs/.
-cp -r /config/openhab/* /data/openhab/.
-cp -r /config/mosquitto/* /data/mosquitto/.
-cp -r /config/signal/* /data/signal/.
-
-/usr/sbin/sshd -p 22 &
-
 echo "This is where your application would start..."
 while : ; do
+  cd /data
+  git pull
   echo "waiting"
   sleep 60
 done
