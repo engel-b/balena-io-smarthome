e882173c2f8190410555a75a44513655a0ba9ea3
Björn Engel <bjoern.engel@cgi.com>
2019-02-26 21:44:02 +0100

    config-Dockerfile

--- config/app.sh
+++ config/app.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo "This is where your application would start..."
+#git clone ssh://git@192.168.150.101:/volume1/homes/git/repos/smarthome-config
+#cd smarthome-config
 while : ; do
   cd /data
 
